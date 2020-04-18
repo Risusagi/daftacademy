b18eef8e780ab6f8bce376243b290b8f9ffdf9f9
Risusagi <anastazja199707@gmail.com>
2020-04-18 21:00:58 +0300

    Added scroll to top icon

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
 import "./styles/style.scss";
+
+(function () {
+  // show scroll to top icon when screen is scrolled down more than 2/3 of its height
+  const btnClasses = document.querySelector(".scroll-to-top").classList;
+
+  window.addEventListener("scroll", () => {
+    const under = window.pageYOffset > (window.innerHeight * 2) / 3;
+
+    under ? btnClasses.remove("hidden") : btnClasses.add("hidden");
+  });
+})();
