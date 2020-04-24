af0fb6c0c2a340c0d90d882d51b35ae6f70e277b
Risusagi <anastazja199707@gmail.com>
2020-04-24 18:52:19 +0300

    Added module pattern, changed scroll to top

--- src/index.js
+++ src/index.js
@@ -1,12 +1,23 @@
 import "./styles/style.scss";
 
-(function () {
-  // show scroll to top icon when screen is scrolled down more than 2/3 of its height
-  const btnClasses = document.querySelector(".scroll-to-top").classList;
+window.addEventListener("DOMContentLoaded", () => {
+  const module = () => {
+    const scrollToTopClasses = document.querySelector(".scroll-to-top")
+      .classList;
 
-  window.addEventListener("scroll", () => {
-    const under = window.pageYOffset > (window.innerHeight * 2) / 3;
+    // show scroll to top icon when screen is scrolled down more than 1/2 of its height
+    const handleScrollToTop = () => {
+      const under = window.pageYOffset > window.innerHeight / 2;
 
-    under ? btnClasses.remove("hidden") : btnClasses.add("hidden");
-  });
-})();
+      under
+        ? scrollToTopClasses.remove("hidden")
+        : scrollToTopClasses.add("hidden");
+    };
+
+    return {
+      handleScrollToTop,
+    };
+  };
+
+  window.addEventListener("scroll", module().handleScrollToTop);
+});
