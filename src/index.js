d52fa117d822513ce82135018e02ee776346bc4f
Risusagi <anastazja199707@gmail.com>
2020-04-25 13:58:18 +0300

    Added loading of more products from a new arrivals list

--- src/index.js
+++ src/index.js
@@ -21,13 +21,57 @@ window.addEventListener("DOMContentLoaded", () => {
       copyrightDate.textContent = new Date().getFullYear();
     };
 
+    // card of a product from a new arrivals list
+    const productCard = () => {
+      const card = document.createElement("div");
+      card.classList.add(
+        "new__card",
+        "card",
+        "col-12",
+        "col-sm-6",
+        "col-md-4",
+        "col-lg-3"
+      );
+      card.innerHTML = `
+          <a href="#">
+            <img
+              class="card-img-top w-90"
+              src="https://picsum.photos/309/390"
+              alt="Coat"
+            />
+          </a>
+          <div class="card-body">
+            <a class="card-text new__product-name" href="#">Coat</a>
+            <span class="card-text price">$49.99</span>
+          </div>
+      `;
+      return card;
+    };
+
+    // load next 12 product cards on "All products" button click
+    const showMoreProducts = () => {
+      const productsContainer = document.querySelector(".products-container");
+
+      document.querySelector(".show-more-btn").addEventListener("click", () => {
+        const fragment = document.createDocumentFragment();
+
+        for (let i = 0; i < 12; i++) {
+          fragment.appendChild(productCard());
+        }
+
+        productsContainer.appendChild(fragment);
+      });
+    };
+
     return {
       handleScrollToTop,
       handleCopyrightDate,
+      showMoreProducts,
     };
   };
 
   module().handleCopyrightDate();
+  module().showMoreProducts();
 
   window.addEventListener("scroll", module().handleScrollToTop);
 });
