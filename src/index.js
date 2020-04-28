4a31cdf16721e1b449f95816e8e0050220b397c1
Risusagi <anastazja199707@gmail.com>
2020-04-28 16:51:43 +0300

    Interactivity for carousel

--- src/index.js
+++ src/index.js
@@ -3,6 +3,12 @@ import {
   handleScrollToTop,
   handleCopyrightDate,
   showMoreProducts,
+  next,
+  prev,
+  handleMouseDown,
+  handleMouseUp,
+  handleTouchStart,
+  handleTouchEnd,
 } from "./js/methods";
 
 window.addEventListener("DOMContentLoaded", () => {
@@ -11,4 +17,23 @@ window.addEventListener("DOMContentLoaded", () => {
 
   const moreBtn = document.querySelector(".show-more-btn");
   moreBtn.addEventListener("click", showMoreProducts);
+
+  /* handle carousel */
+  document
+    .querySelector(".main__carousel-control--next")
+    .addEventListener("click", next);
+
+  document
+    .querySelector(".main__carousel-control--prev")
+    .addEventListener("click", prev);
+
+  const wrap = document.querySelector(".main__carousel-wrapper");
+
+  wrap.addEventListener("mousedown", handleMouseDown);
+
+  wrap.addEventListener("mouseup", handleMouseUp);
+
+  wrap.addEventListener("touchstart", handleTouchStart);
+
+  wrap.addEventListener("touchend", handleTouchEnd);
 });
--- src/js/methods.js
+++ src/js/methods.js
@@ -57,3 +57,72 @@ export const showMoreProducts = () => {
 
   productsContainer.appendChild(fragment);
 };
+
+/* handle carousel */
+const slidesContainer = document.querySelector(".main__carousel-slides");
+const slidesAmount = document.querySelectorAll(".main__carousel-card").length;
+const fullWidth = parseFloat(
+  getComputedStyle(document.querySelector(".main__carousel-slider")).width
+);
+// width of img plus 1rem padding
+const slideWidth = 268 + 16;
+// amount of visible slides
+const amount = fullWidth / slideWidth;
+// amount of slides fully visible (100% of their width)
+const int = Math.floor(amount);
+// part of the last visible slide that is not visible
+const slideSlice = 1 - (amount - int);
+
+let counter = 0;
+
+export const next = () => {
+  if (Math.abs(counter) + amount >= slidesAmount) return;
+
+  // for cases when carousel shows some amount of slides plus part of one slide
+  // if only part of the last slide left to scroll, scroll carousel for this part (smaller than slides full width)
+  if (Math.abs(counter) === slidesAmount - int - 1) {
+    counter -= slideSlice;
+  } else {
+    counter--;
+  }
+
+  slidesContainer.style.transform = `translateX(${slideWidth * counter}px)`;
+};
+
+export const prev = () => {
+  if (counter === 0) return;
+  if (slideSlice >= Math.abs(counter)) {
+    counter = 0;
+  } else {
+    counter++;
+  }
+
+  slidesContainer.style.transform = `translateX(${slideWidth * counter}px)`;
+};
+
+let startX = 0;
+let endX = 0;
+
+// move slides in the same direction as mouse was move / screen was slided
+const handleSlide = () => {
+  // left to right
+  if (startX < endX) {
+    prev();
+  } else if (startX > endX) {
+    next();
+  }
+};
+
+export const handleMouseDown = (e) => (startX = e.clientX);
+
+export const handleMouseUp = (e) => {
+  endX = e.clientX;
+  handleSlide();
+};
+
+export const handleTouchStart = (e) => (startX = e.touches[0].clientX);
+
+export const handleTouchEnd = (e) => {
+  endX = e.changedTouches[0].pageX;
+  handleSlide();
+};
